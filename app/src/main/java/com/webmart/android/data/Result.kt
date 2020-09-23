2254e8f1737accf19e60a60914767aac9ee34393
haseebcgt <abdul.haseeb@cgtindia.in>
2020-09-23 19:06:22 +0530

    Added util and result

--- app/src/main/java/com/webmart/android/data/Result.kt
+++ app/src/main/java/com/webmart/android/data/Result.kt
@@ -1,3 +1,8 @@
-package com.webmart.android.data
+package com.android.webmart.data
 
-class Result
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+}
---
+++ app/src/main/java/com/webmart/android/util/constants/Constants.kt
@@ -0,0 +1,2 @@
+package com.webmart.android.util.constants
+
