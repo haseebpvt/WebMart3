c8d77dbb979d6fcfaa45ea887a372981457cf238
haseebcgt <abdul.haseeb@cgtindia.in>
2020-09-23 18:55:13 +0530

    Bottom navigation completed

--- app/src/main/java/com/webmart/android/ui/MainActivity.kt
+++ app/src/main/java/com/webmart/android/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.webmart.android.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.webmart.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home_fragment, R.id.favorite_fragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
